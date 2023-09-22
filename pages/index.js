3c91d7266dc1661cd3f8c1fd53c3aa5f47f9016e
Dorottya Ferencz <retyidoro@yahoo.com>
2023-09-22 12:29:16 +0300

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>Blank Virtuous Wombat</title>
+          <meta property="og:title" content="Blank Virtuous Wombat" />
+        </Head>
+        <h1>Heading</h1>
+        <div className="home-container1"></div>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+          .home-container1 {
+            flex: 0 0 auto;
+            width: 200px;
+            border: 2px dashed rgba(120, 120, 120, 0.4);
+            height: 100px;
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
